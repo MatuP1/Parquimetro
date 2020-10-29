1396e7f79fdac11dca8e7da175c6caea15dab33b
Matias <matiaspippig@gmail.com>
2020-10-29 20:09:51 -0300

    Revert "Agregados botones de back y msj error en los login"
    This reverts commit 24f02d18260c8c8c03afe39e3c9a9f9e2051bc6a.

--- src/parquimetro/PanelIngresoAdmin.java
+++ src/parquimetro/PanelIngresoAdmin.java
@@ -6,16 +6,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
 
 import javax.swing.JPasswordField;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 public class PanelIngresoAdmin extends JPanel{
 	private static final long serialVersionUID = 1L;
@@ -41,6 +37,7 @@ public class PanelIngresoAdmin extends JPanel{
 		panel.setBounds(0, 0, 200, 200);
 		
 		passwordField = new JPasswordField(30);
+		//String password = new String(passwordField.getPassword());
 		passwordField.addKeyListener(LPass);
 		
 		JLabel labelIngreso = new JLabel("Ingrese su contraseña");
@@ -48,17 +45,8 @@ public class PanelIngresoAdmin extends JPanel{
 		panel.add(labelIngreso);
 		panel.add(passwordField);
 		
-		this.add(panel);
-		
-		JButton btnBack = new JButton("Volver a Inicio");
-		btnBack.addMouseListener(new MouseAdapter() {
- 			@Override
- 			public void mouseClicked(MouseEvent e) {
- 				vPrincipal.volverPanelInicial();
- 			}
- 		});
-		
-		this.add(btnBack);
+		//System.out.println("El pass es: " + password);
+		this.add(panel);	
 	}
 	
 	
@@ -67,18 +55,13 @@ public class PanelIngresoAdmin extends JPanel{
 		public void keyPressed(KeyEvent e) {
 			pass = new String(passwordField.getPassword());
 			System.out.println("El pass es: " + pass);
-			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-				if(logica.checkAdmin(pass)) {
+			if(e.getKeyCode() == KeyEvent.VK_ENTER && logica.checkAdmin(pass)) {
 				System.out.println("Checkadmin devolvio true");
 				logica.connectAdmin(pass);
 				ConsultasAdmin ca = new ConsultasAdmin(vPrincipal);
 				vPrincipal.cambiarFrame(ca);
-				}else {
-					JOptionPane.showMessageDialog(null, "Contraseña incorrecta","Mensaje Error", JOptionPane.WARNING_MESSAGE);
-				}
 			}
 		}
-	
 
 		@Override
 		public void keyReleased(KeyEvent arg0) {
--- src/parquimetro/PanelIngresoInspector.java
+++ src/parquimetro/PanelIngresoInspector.java
@@ -4,10 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -63,17 +60,7 @@ public class PanelIngresoInspector extends JPanel {
 		panel.add(panelUser,BorderLayout.NORTH);
 		panel.add(panelPassword,BorderLayout.CENTER);
 		
-		JButton btnBack = new JButton("Volver a Inicio");
-		btnBack.addMouseListener(new MouseAdapter() {
- 			@Override
- 			public void mouseClicked(MouseEvent e) {
- 				logica.desconectar();
- 				vPrincipal.volverPanelInicial();
- 			}
- 		});
-		
 		this.add(panel);
-		this.add(btnBack);
 	}
 	
 	private class OyentePass implements KeyListener{
