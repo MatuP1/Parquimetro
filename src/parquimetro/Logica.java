b5f6eb0adcf83cc47708d629182af7f4e3c7a4f1
MatuP <matiaspippig@gmail.com>
2020-10-29 12:11:18 -0300

    Sigo sin saber que pasa

--- src/parquimetro/Logica.java
+++ src/parquimetro/Logica.java
@@ -151,6 +151,7 @@ public class Logica {
 			try {
 				if (insp.next())
 					accede=true;
+					System.out.println(insp.getString("legajo")+insp.getString("password") );
 			} catch (SQLException e) {
 				System.out.println("Error en wasNull de inspector");
 				e.printStackTrace();
