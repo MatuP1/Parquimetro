5eb8367e4f7cb918e92187214ed6c0dcb48cfdb3
NahuelMaika <nahuelmaika@gmail.com>
2020-11-23 19:22:17 -0300

    Casi Finish Punto4 Proyecto3

--- src/parquimetro/PanelIngresoUsuario.java
+++ src/parquimetro/PanelIngresoUsuario.java
@@ -36,6 +36,7 @@ public class PanelIngresoUsuario extends javax.swing.JInternalFrame{
 	private DBTable table_tarjetas;
 	private DBTable table_ubicaciones;
 	private DBTable table_parquimetros;
+	private DBTable table_res;
 	private int id_tarj;
 	private int id_parq;
 	
@@ -45,7 +46,7 @@ public class PanelIngresoUsuario extends javax.swing.JInternalFrame{
 		Alto = v.getAlto();
 		logica = vPrincipal.getLogica();
 		
-		//this.setBounds(0, 0, Ancho, Alto);
+		this.setBounds(0, 0, Ancho, Alto);
         setVisible(true);
         this.setTitle("Ingreso Usuario");
         this.setClosable(false);
@@ -55,45 +56,47 @@ public class PanelIngresoUsuario extends javax.swing.JInternalFrame{
         table_parquimetros = new DBTable();
  		table_ubicaciones= new DBTable();
  		table_tarjetas =new DBTable();
+ 		table_res = new DBTable();
  		
         JScrollPane scrollPaneUbicaciones = new JScrollPane();
  		scrollPaneUbicaciones.setEnabled(true);
- 		scrollPaneUbicaciones.setBounds(170, 34, 567, 137);
+ 		scrollPaneUbicaciones.setBounds(111, 34, 567, 137);
  		getContentPane().add(scrollPaneUbicaciones);
  		
  		JScrollPane scrollPaneParquimetros = new JScrollPane();
  		scrollPaneParquimetros.setEnabled(true);
- 		scrollPaneParquimetros.setBounds(170, 210, 567, 137);
+ 		scrollPaneParquimetros.setBounds(111, 210, 567, 137);
  		getContentPane().add(scrollPaneParquimetros);
  		
  		JScrollPane scrollPaneTarjetas = new JScrollPane();
  		scrollPaneTarjetas.setEnabled(true);
- 		scrollPaneTarjetas.setBounds(170, 391, 567, 137);
+ 		scrollPaneTarjetas.setBounds(111, 391, 567, 137);
  		getContentPane().add(scrollPaneTarjetas);
  		
  		JButton btnIngreso = new JButton("Ingresar/Salir de Estacionamiento");
- 		btnIngreso.setBounds(353, 553, 199, 55);
+ 		btnIngreso.setBounds(289, 566, 199, 55);
  		btnIngreso.setEnabled(false);
  		getContentPane().add(btnIngreso);
  		
  		JLabel lblNewLabel_1 = new JLabel("Parquimetros");
  		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
  		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
- 		lblNewLabel_1.setBounds(170, 182, 567, 21);
+ 		lblNewLabel_1.setBounds(111, 182, 567, 21);
  		getContentPane().add(lblNewLabel_1);
  		
      	JLabel lblNewLabel_2 = new JLabel("Ubicaciones");
      	lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
      	lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-     	lblNewLabel_2.setBounds(170, 11, 567, 21);
+     	lblNewLabel_2.setBounds(111, 11, 567, 21);
      	getContentPane().add(lblNewLabel_2);
      	
  		JLabel lblNewLabel_3 = new JLabel("Tarjetas");
      	lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
      	lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-     	lblNewLabel_3.setBounds(170, 363, 567, 21);
+     	lblNewLabel_3.setBounds(111, 363, 567, 21);
      	getContentPane().add(lblNewLabel_3);
  		
+     	
  		try {
  			String sql="Select * from ubicaciones"; //HAY QUE CHECKEAR SI ESTA BIEN
  			Statement st=logica.getConnection().createStatement();
@@ -114,7 +117,7 @@ public class PanelIngresoUsuario extends javax.swing.JInternalFrame{
  		table_ubicaciones.addMouseListener(new MouseAdapter() {
  			@Override
  			public void mouseClicked(MouseEvent e) {
- 				
+ 				table_tarjetas.setVisible(false);
  				String calle,altura;
  				btnIngreso.setEnabled(false);
 				int fila = table_ubicaciones.getSelectedRow();
@@ -148,6 +151,7 @@ public class PanelIngresoUsuario extends javax.swing.JInternalFrame{
 				Statement st_tarj = logica.getConnection().createStatement();
 				ResultSet rs_tarj = st_tarj.executeQuery(sql_tarj);
 				table_tarjetas.refresh(rs_tarj);
+				table_tarjetas.setVisible(true);
 				} catch (SQLException e1) {
 	 				// en caso de error, se muestra la causa en la consola
 			         System.out.println("SQLException: " + e1.getMessage());
@@ -185,9 +189,32 @@ public class PanelIngresoUsuario extends javax.swing.JInternalFrame{
                 	id_tarj = (int)table_tarjetas.getValueAt(fila_tarj, 0);
                 	id_parq = (int)table_parquimetros.getValueAt(fila_parq, 0);
                 	
+                	ResultSet res = logica.conexion_parq(id_tarj, id_parq);
+                	table_res = new DBTable();
+                	try {
+						table_res.refresh(res);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+                	
+                	int fila_res=table_res.getSelectedRow();
+                	String tipo_ope = table_res.getValueAt(fila_res, 0).toString();
+                	String tipo_res = table_res.getValueAt(fila_res, 1).toString();
+                	int minutos_restantes = (int)table_res.getValueAt(fila_res, 2); //Es devuelto en minutos
+                	
+                	int horas=minutos_restantes/60;
+                	int minutos=minutos_restantes-horas*60;
                 	
                 	
-                	
+                	if(tipo_ope == "apertura" && tipo_res == "Ok") {
+                		JOptionPane.showMessageDialog(null, "Se abrio con exito un estacionamiento y le quedan \""+horas+"\":\""+minutos+"\".");
+                	}else if(tipo_ope == "apertura" && tipo_res == "Fail") {
+                		JOptionPane.showMessageDialog(null, "No se pudo abrir un estacionamiento con dicha tarjeta");
+                	}else if(tipo_ope == "cierre" && tipo_res == "Ok") {
+                		JOptionPane.showMessageDialog(null, "Se cerro con exito un estacionamiento y estuvo estacionado \""+horas+"\":\""+minutos+"\".");
+                	}else if(tipo_ope == "cierre" && tipo_res == "Fail") {
+                		JOptionPane.showMessageDialog(null, "No se pudo cerrar un estacionamiento con dicha tarjeta");
+                	}
         	
               }
            });
